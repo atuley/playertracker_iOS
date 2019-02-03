de28e4364ec224d6d45eec716f2c5d4162df3697
Alex Tuley <alex.intern@gaslight.co>
2019-02-03 17:29:10 -0500

    init

---
+++ playertracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  playertracker
+//
+//  Created by Alex Tuley on 2/3/19.
+//  Copyright © 2019 Alex Tuley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
