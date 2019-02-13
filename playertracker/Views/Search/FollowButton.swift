c79606d02bba5109da8ec636e88eaaafe91553a7
Alex Tuley <alex.intern@gaslight.co>
2019-02-12 19:41:25 -0500

    Move files around a little bit; Create FollowButton file

---
+++ playertracker/Views/Search/FollowButton.swift
@@ -0,0 +1,21 @@
+//
+//  FollowButton.swift
+//  playertracker
+//
+//  Created by Alex Tuley on 2/12/19.
+//  Copyright © 2019 Alex Tuley. All rights reserved.
+//
+
+import UIKit
+
+class FollowButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
